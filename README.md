a9637e61279ec03f631d3d6508b7bdfc71a405b8
austinbhan <austinbhan@outlook.com>
2023-11-16 16:22:55 -0800

    First bug, post commands

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pernapp
 Following along with freecodecamp's PERN tutorial 
+
+11/16/23
+Bug: SQL table exists in postgres, SQL commands in beekeeper will update data. VSCode post commands not fetching correctly. Syntax matches tutorial.
+Hypothesis: Is this is a port issue?
+Reference: https://www.youtube.com/watch?v=ldYcgPKEZC8&t=1118s, 20:24
--- server/index.js
+++ server/index.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+
+// create todo
+app.post("/todos", async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES($1) RETURNING *", 
+            [description]
+            );
+    
+            res.json(newTodo);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// get all todos
+
+// get a todo
+
+// update todo
+
+// delete todo
 
 app.listen(5432, () => {
     console.log("server has started on port 5432");
