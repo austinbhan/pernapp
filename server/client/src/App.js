9b311d64c4631c9d290275d6975017f7504f2696
austinbhan <austinbhan@outlook.com>
2023-11-27 09:12:17 -0800

    Self made add function for frontend

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,10 +1,16 @@
-
 import './App.css';
 import React, { Fragment } from "react";
 
+import InputTodo from './components/InputToDo';
+
 function App() {
+
+
   return (
     <Fragment>
+      <div className = "container">
+        <InputTodo />
+      </div>
     </Fragment> 
     
   );
